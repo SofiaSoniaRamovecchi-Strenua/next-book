aa206035cd7b254705bd668e362b156b7c1fbbbc
SofiaSoniaRamovecchi-Strenua <strenuaramovecchi@gmail.com>
2025-04-22 01:18:03 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,40 @@
-// src/index.ts
 import express from 'express';
 
 const app: express.Application = express();
 const port = 3000;
 
-app.use(express.text());
+app.use(express.text());  // Para aceptar texto en el cuerpo de la solicitud
 
-app.listen(port, () => {
-  console.log(`server is listening on ${port}`);
-});
-
-// Homepage
+// Home route
 app.get('/', (req: express.Request, res: express.Response) => {
   res.status(200).send("Hello World!");
 });
 
-// GET
+// GET route
 app.get('/get', (req: express.Request, res: express.Response) => {
   res.status(200).header("x-get-header", "get-header-value").send("get-response-from-compute");
 });
 
-//POST
+// POST route
 app.post('/post', (req: express.Request, res: express.Response) => {
   res.status(200).header("x-post-header", "post-header-value").send(req.body.toString());
 });
 
-//PUT
+// PUT route
 app.put('/put', (req: express.Request, res: express.Response) => {
   res.status(200).header("x-put-header", "put-header-value").send(req.body.toString());
 });
 
-//PATCH
+// PATCH route
 app.patch('/patch', (req: express.Request, res: express.Response) => {
   res.status(200).header("x-patch-header", "patch-header-value").send(req.body.toString());
 });
 
-// Delete
+// DELETE route
 app.delete('/delete', (req: express.Request, res: express.Response) => {
   res.status(200).header("x-delete-header", "delete-header-value").send();
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
